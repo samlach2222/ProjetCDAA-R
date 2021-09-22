7d5ec7c8f5cf2ec346c2b332143672437e72b926
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2021-09-22 14:31:03 +0200

    Création de classe FicheContact
    Création de la classe FicheContact avec ses variables, constructeur et fonctions
    /!\ Manque la récupération d'une id dans le constructeur

---
+++ CDAA-R/LOGIC/fichecontact.cpp
@@ -0,0 +1,103 @@
+#include "fichecontact.h"
+
+FicheContact::FicheContact(std::string nom, std::string prenom, std::string entreprise, std::string mail, std::string telephone, QImage photo, Horodatage dateCreation)
+{
+    this->id = 0;  //TODO: récupérer le plus grand id unique
+    this->nom = nom;
+    this->prenom = prenom;
+    this->entreprise = entreprise;
+    this->mail = mail;
+    this->telephone = telephone;
+    this->photo = photo;
+    this->dateCreation = dateCreation;
+}
+
+int FicheContact::getId()
+{
+    return id;
+}
+
+std::string FicheContact::getNom()
+{
+    return nom;
+}
+
+void FicheContact::setNom(std::string newNom)
+{
+    nom = newNom;
+}
+
+std::string FicheContact::getPrenom()
+{
+    return prenom;
+}
+
+void FicheContact::setPrenom(std::string newPrenom)
+{
+    prenom = newPrenom;
+}
+
+std::string FicheContact::getEntreprise()
+{
+    return entreprise;
+}
+
+void FicheContact::setEntreprise(std::string newEntreprise)
+{
+    entreprise = newEntreprise;
+}
+
+std::string FicheContact::getMail()
+{
+    return mail;
+}
+
+void FicheContact::setMail(std::string newMail)
+{
+    mail = newMail;
+}
+
+std::string FicheContact::getTelephone()
+{
+    return telephone;
+}
+
+void FicheContact::setTelephone(std::string newTelephone)
+{
+    telephone = newTelephone;
+}
+
+QImage FicheContact::getPhoto()
+{
+    return photo;
+}
+
+void FicheContact::setPhoto(QImage newPhoto)
+{
+    photo = newPhoto;
+}
+
+Horodatage FicheContact::getDateCreation()
+{
+    return dateCreation;
+}
+
+void FicheContact::setDateCreation(Horodatage newDateCreation)
+{
+    dateCreation = newDateCreation;
+}
+
+std::vector<Interaction> FicheContact::GetListInteraction()
+{
+    return listInteraction;
+}
+
+void FicheContact::AddInteraction(Interaction i)
+{
+    listInteraction.push_back(i);
+}
+
+void FicheContact::RemoveInteraction(Interaction i)
+{
+    std::remove_if(listInteraction.begin(), listInteraction.end(), [](Interaction currentI){return currentI == i;});
+}
---
+++ CDAA-R/LOGIC/fichecontact.h
@@ -0,0 +1,44 @@
+#ifndef FICHECONTACT_H
+#define FICHECONTACT_H
+#include <string>
+#include <QImage>
+#include <vector>
+
+class FicheContact
+{
+    private:
+        int id;  //Readonly
+        std::string nom;
+        std::string prenom;
+        std::string entreprise;
+        std::string mail;
+        std::string telephone;
+        QImage photo;
+        Horodatage dateCreation;
+        std::vector<Interaction> listInteraction;
+    public:
+        FicheContact(std::string nom, std::string prenom, std::string entreprise, std::string mail, std::string telephone, QImage photo, Horodatage dateCreation);
+
+        //Getters & Setters
+        int getId();
+        std::string getNom();
+        void setNom(std::string newNom);
+        std::string getPrenom();
+        void setPrenom(std::string newPrenom);
+        std::string getEntreprise();
+        void setEntreprise(std::string newEntreprise);
+        std::string getMail();
+        void setMail(std::string newMail);
+        std::string getTelephone();
+        void setTelephone(std::string newTelephone);
+        QImage getPhoto();
+        void setPhoto(QImage newPhoto);
+        Horodatage getDateCreation();
+        void setDateCreation(Horodatage newDateCreation);
+
+        std::vector<Interaction> GetListInteraction();
+        void AddInteraction(Interaction i);
+        void RemoveInteraction(Interaction i);
+};
+
+#endif // FICHECONTACT_H
