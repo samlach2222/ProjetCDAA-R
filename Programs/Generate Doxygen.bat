de233644398f10bc592766db65bb005cf86bece1
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2021-10-09 17:45:40 +0200

    Précisions sur l'ajout de CDAA-R.qch

--- Programs/Generate Doxygen.bat
+++ Programs/Generate Doxygen.bat
@@ -8,6 +8,6 @@ echo.
 "C:\Qt\5.12.11\mingw73_64\bin\qhelpgenerator.exe" ..\Doxygen\html\index.qhp -o ..\Doxygen\CDAA-R.qch
 echo.
 echo.
-echo Ajoutez la documentation (CDAA-R.qch) dans Qt Creator depuis Outils-^>Options...-^>Aide-^>Documentation
+echo Nécessaire qu'une seule fois : Ajoutez la documentation CDAA-R.qch (se trouvant dans le dossier Doxygen) dans Qt Creator depuis Outils-^>Options...-^>Aide-^>Documentation
 echo Déplacer le curseur de la souris sur une fonction affiche un tooltip, la documentation complète s'affiche avec F1
 TIMEOUT 8
