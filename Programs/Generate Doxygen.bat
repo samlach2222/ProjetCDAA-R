b30af9716c4b5e9ab2146445af96c400961c48ee
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2021-10-27 11:17:38 +0200

    Corrige else du GenerateDoxygen

--- Programs/Generate Doxygen.bat
+++ Programs/Generate Doxygen.bat
@@ -4,7 +4,8 @@ cd "%~dp0"
 echo.
 python doxygen2qtcreator_modified.py ..\Doxygen\html
 echo.
-if %username% == samlach22 ("D:\LOGICIELS\QT\5.12.11\mingw73_64\bin\qhelpgenerator.exe" ..\Doxygen\html\index.qhp -o ..\Doxygen\CDAA-R.qch) else ("C:\Qt\5.12.11\mingw73_64\bin\qhelpgenerator.exe" ..\Doxygen\html\index.qhp -o ..\Doxygen\CDAA-R.qch ; chcp 65001)
+if %username% == samlach22 ("D:\LOGICIELS\QT\5.12.11\mingw73_64\bin\qhelpgenerator.exe" ..\Doxygen\html\index.qhp -o ..\Doxygen\CDAA-R.qch) else  ("C:\Qt\5.12.11\mingw73_64\bin\qhelpgenerator.exe" ..\Doxygen\html\index.qhp -o ..\Doxygen\CDAA-R.qch
+chcp 65001)
 echo.
 echo.
 echo Nécessaire qu'une seule fois : Ajoutez la documentation CDAA-R.qch (se trouvant dans le dossier Doxygen) dans Qt Creator depuis Outils-^>Options...-^>Aide-^>Documentation
