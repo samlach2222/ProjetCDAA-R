68ae9b2e3ff24ef5900e60e42fa39a7885f68cf2
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2021-12-05 03:34:14 +0100

    Met à jour Doxygen

--- Doxygen/html/search/all_2.js
+++ Doxygen/html/search/all_2.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['changementstatuscb_0',['changementStatusCB',['../class_u_i___request_contact.html#aa9744f1f9f426dcaae1b2e2e0e9e1efd',1,'UI_RequestContact']]],
   ['chooseimage_1',['ChooseImage',['../class_main_window.html#aad1ebd0dab99fb187071fd3f0cf9a8a9',1,'MainWindow']]],
-  ['createcontact_2',['CreateContact',['../class_database_storage.html#ac5eba38bd8533c59e012e67ba5748766',1,'DatabaseStorage']]],
-  ['createinteractionandtags_3',['CreateInteractionAndTags',['../class_database_storage.html#ab57d36105e93940907b0cbdc04e9685c',1,'DatabaseStorage']]],
-  ['createlog_4',['CreateLog',['../class_database_storage.html#ad843d29783b7d9a64b7b0f2658b8bb0f',1,'DatabaseStorage']]]
+  ['closebdd_2',['CloseBDD',['../class_database_storage.html#a3d409da175235d9d07e32b415c19f6c6',1,'DatabaseStorage']]],
+  ['createcontact_3',['CreateContact',['../class_database_storage.html#ac5eba38bd8533c59e012e67ba5748766',1,'DatabaseStorage']]],
+  ['createinteractionandtags_4',['CreateInteractionAndTags',['../class_database_storage.html#a6181e4452d3f6976f0e2f215b3cfec32',1,'DatabaseStorage']]],
+  ['createlog_5',['CreateLog',['../class_database_storage.html#ad843d29783b7d9a64b7b0f2658b8bb0f',1,'DatabaseStorage']]]
 ];
